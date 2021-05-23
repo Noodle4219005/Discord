379c5a6411dc3f427941c5f0d5c99785ba4d25d2
Noodle4219005 <mbdd0921@gmail.com>
2021-05-23 13:18:44 +0800

    settings.json

--- A_Noodle.py
+++ A_Noodle.py
@@ -1,8 +1,14 @@
 import discord
 from discord.ext import commands
 
+import json
+import random
+
+with open('settings.json','r', encoding = 'utf8') as jfile:
+    jdata = json.load(jfile)
+
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='[', intents=intents)
 client = discord.Client(intents=intents)
 
 @bot.event
@@ -11,12 +17,21 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(845722197200601109)
+    channel = bot.get_channel(int(jdata['WelcomeChannel']))
     await channel.send(f"{member}join!")
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(845722197200601109)
+    channel = bot.get_channel(int(jdata['RemoveChannel']))
     await channel.send(f"{member}leave!")
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-bot.run('Token')
+@bot.command()
+async def picture(ctx):
+    random_pic = random.choice(jdata['pic']) 
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+
+bot.run(jdata['TOKEN'])
